445d9cd6aace643eff88b1395a9de703872ed770
Abdurrizqo <59135074+Abdurrizqo@users.noreply.github.com>
2025-01-09 13:21:18 +0800

    fix done

--- resources/views/SuperAdmin/layoutSuperAdmin.blade.php
+++ resources/views/SuperAdmin/layoutSuperAdmin.blade.php
@@ -31,10 +31,18 @@
             </svg>
         </a>
         <a href="list-admin" class="{{ setActiveSegmentWildcardMobile('list-admin') }}">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
                 stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M16 17v-2a4 4 0 10-8 0v2m4-6a4 4 0 100-8 4 4 0 000 8z" />
+            </svg>
+        </a>
+
+        <a href="/logout" class="{{ setActiveSegmentWildcardMobile('logout') }}">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="h-5 w-5 text-white">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
             </svg>
         </a>
     </div>
